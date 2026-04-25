a6dd6931fcb7788613067148c944fe48eab7f006
Joshua Trevino <69660428+joshuaOne9@users.noreply.github.com>
2026-04-25 10:06:54 -0500

    Refine hero subtitle to match specific scripture verse

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ function App() {
   return (
     <div className="hero">
       <h1>Jesus is the Way</h1>
-        <p className="subtitle">The Truth and The Life</p>
+        <p className="subtitle">"I am the way, and the truth, and the life." John 14:6</p>
         <p className="description">
           A place to explore Scripture, encounter Jesus, and seek context from all books related to The Bible</p>
     </div>
